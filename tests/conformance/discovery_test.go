443a15766120af808a1ea8d44d0fea84b8fc6c7a
GawronskaSylwia <71151101+GawronskaSylwia@users.noreply.github.com>
2024-09-09 17:47:27 +0200

    Fix discovery test

--- tests/conformance/discovery_test.go
+++ tests/conformance/discovery_test.go
@@ -2,17 +2,19 @@ package tests
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"starknet-p2p-tests/config"
 	synthetic_node "starknet-p2p-tests/tools"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDiscovery(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
 	defer cancel()
 
 	node1, err := synthetic_node.New(ctx)
@@ -26,14 +28,20 @@ func TestDiscovery(t *testing.T) {
 	err = node2.Connect(ctx, config.TargetPeerAddress)
 	require.NoError(t, err)
 
+	fmt.Printf("Node 1 ID: %s\n", node1.Host.ID())
+	fmt.Printf("Node 2 ID: %s\n", node2.Host.ID())
+
 	// Wait for the DHT to work its magic
-	time.Sleep(60 * time.Second)
+	time.Sleep(100 * time.Second)
 
 	peers1 := node1.Host.Peerstore().Peers()
 	peers2 := node2.Host.Peerstore().Peers()
 
-	require.Contains(t, peers1, node2.Host.ID(), "Node 1 should have discovered Node 2")
-	require.Contains(t, peers2, node1.Host.ID(), "Node 2 should have discovered Node 1")
+	fmt.Printf("Node 1 peers: %v\n", peers1)
+	fmt.Printf("Node 2 peers: %v\n", peers2)
+
+	assert.Contains(t, peers1, node2.Host.ID(), "Node 1 should have discovered Node 2")
+	assert.Contains(t, peers2, node1.Host.ID(), "Node 2 should have discovered Node 1")
 
 	err = node1.Close()
 	require.NoError(t, err)
