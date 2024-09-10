e43fb17034df132dcdd47b5d2af3563b15cf1e89
wojciechos <wojciech.zieba@gmail.com>
2024-09-10 08:44:10 +0200

    Discovery test (#3)
    * Add p2p discover test
    
    * Fix discovery test
    
    * Clean up tests
    
    * Use go's benchmarking for perf tests
    
    * dsf
    
    * Make perf test framework
    
    * use external target node for pipeline run
    
    ---------

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ var (
 func init() {
 	//TargetPeerAddress = getEnv("TARGET_PEER_ADDRESS", "/ip4/35.237.66.77/tcp/7777/p2p/12D3KooWR8ikUDiinyE5wgdYiqsdLfJRsBDYKGii6L3oyoipVEaV")
 	TargetPeerAddress = getEnv("TARGET_PEER_ADDRESS", "/ip4/127.0.0.1/tcp/7777/p2p/12D3KooWLBUjEPyTiACzQZ3K1oqBXRqHwRFvAUHrm561pWWbJkYf")
-	timeoutStr := getEnv("DEFAULT_TEST_TIMEOUT", "3000s")
+	timeoutStr := getEnv("DEFAULT_TEST_TIMEOUT", "300s")
 	var err error
 	DefaultTestTimeout, err = time.ParseDuration(timeoutStr)
 	if err != nil {
